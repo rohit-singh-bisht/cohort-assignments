e83bffba9a90efd34bb09abefa23885ca6592ad1
rohit-singh-bisht <thersbisht@gmail.com>
2023-12-20 10:26:52 +0530

    file cleaner

--- week-2/01-async-js/medium/fileCleaner.js
+++ week-2/01-async-js/medium/fileCleaner.js
@@ -2,10 +2,14 @@ const fs = require("fs");
 
 const cleanFile = (filePath) => {
   // read the file using fs library.
-  const file = fs.readFile(filePath, "utf-8", (err, data) => {
+  fs.readFile(filePath, "utf-8", (err, data) => {
     if (err) throw new Error("Error Reading the file ");
+
+    // beautify the file.
     const cleanedFille = data.replace(/\s+/g, " ");
-    fs.writeFile(filePath, cleanFile, (err) => {
+
+    // rewrite the file
+    fs.writeFile(filePath, cleanedFille, (err) => {
       if (err) throw new Error("Error Writing the file");
       console.log("File updated successfully");
     });
